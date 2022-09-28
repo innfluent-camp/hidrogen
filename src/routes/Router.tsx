689a8b09e5b5594338597493da83a869b0356559
Mateo Upegui Borja <upeguiborja@gmail.com>
2022-09-27 19:50:03 -0500

    chore(dashboard): add dashboard route boilerplate

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from 'routes/login';
+import Dashboard from './dashboard';
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
